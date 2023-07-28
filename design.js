b2b720eb402ad0685dbf7468ad9c2514e344a84f
PeetaniVijayaBhavani <125951404+PeetanivijayaBhavani@users.noreply.github.com>
2023-07-28 19:33:52 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,27 @@
+ReactDOM.render(
+<center><div>
+    <h1><b>Biography</b></h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9-Gt4oi1bHC49dBJQE-WlGrfveS_66zBpMQ&usqp=CAU"/>
+   
+    <h2>Name:Peetani Savithri</h2>
+    <h3>Date Of Birth:25/2/1985</h3>
+    <h3>Place:Kopparru</h3>
+    <h3>Gender:Female</h3>
+    <h3>Profession:Village Assistent(VO-A)</h3>
+    <h4>“My mother is my role model, she inspires and motivates me to grow without any barriers”</h4>
+   <div><p id="description">
+   Super humans. We read about them, watch them and admire them in the virtual world , but I live with a superhuman.
+   She is my mother.I am Vijaya Bhavani, a 20-year-old student.My mother, Mrs Peetani Savithri, is my role model. 
+   She inspires and motivates me to grow without any barriers. It seems to me that this woman looks at life as a
+   challenge and wholeheartedly intends to seize the day every day.  
+   My mother looked after me and supported me in every part of my life. Right from childhood she has been with me 
+   like my shadow. It had been riddled with hurdles and difficulties, but she has managed to cross them all to because
+   of the amazing and independent person she is.
+   A mother is the truest friend we have, when trials heavy and sudden fall upon us; when adversity takes the place of 
+   prosperity; when friends desert us; when trouble thickens around us, still will she cling to us, and endeavor by her 
+   kind precepts and counsels to dissipate the clouds of darkness, and cause peace to return to our hearts.
+   </p></div>
+
+    
+</div></center>
+,document.getElementById("bio"));
